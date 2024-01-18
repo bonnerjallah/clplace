9d2459c0f59187dd2686216c1dcd8eb818447734
bonnerjallah <121054718+bonnerjallah@users.noreply.github.com>
2024-01-17 22:37:40 -0500

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider, } from "react-router-dom"
 
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
+import Gallery from "./pages/Gallery"
+import AboutUs from "./pages/AboutUs"
+import ContactUs from "./pages/ContactUs"
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Navbar />}>
+      <Route path="/" element={<Home />} />
+      <Route path="Gallery" element={<Gallery /> } />
+      <Route path="AboutUs" element={<AboutUs /> } />
+      <Route path="ContactUs" element={<ContactUs /> } />
+    </Route>
+  )
+)
 
 function App() {
 
   return (
     <>
-      hellow world
+      <RouterProvider router={router} />
     </>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
-  base: '/clplace/',
+  base:'./',
   plugins: [react()],
-})
+});
