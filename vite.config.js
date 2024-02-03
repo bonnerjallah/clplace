c83456888c58a5805f93b337093cecaa45c307e2
bonnerjallah <121054718+bonnerjallah@users.noreply.github.com>
2024-02-02 22:39:31 -0500

    modified index.html

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,8 @@ import react from '@vitejs/plugin-react';
 
 
 //https://vitejs.dev/config/
-const isProduction = process.env.NODE_ENV === "production";
 
 
 export default defineConfig({
-  base: isProduction ? "/clplace/" : "./",
   plugins: [react()],
-  optimizeDeps: {
-    include: ['react', 'react-dom'],
-  },
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom'],
-    },
-  },
 });
